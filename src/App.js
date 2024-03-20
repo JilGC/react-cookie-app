c30967bead3bb9a157154cec8b466f25f9efa083
JilGC <105502236+JilGC@users.noreply.github.com>
2024-03-20 15:57:49 -0300

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,3 +1,74 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function GithubUser({ name, location, avatar }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{location}</p>
+      <img src={avatar} height={150} alt={name} />
+    </div>
+  );
+}
+
+function App() {
+
+  const [data, setData] = useState(null);
+
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/jilgc`)
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
+  if (data)
+    return (
+      <GithubUser
+        name={data.name}
+        location={data.location}
+        avatar={data.avatar_url}
+      />
+    )
+  return <h1>Data</h1>;
+}
+
+export default App;
+
+
+
+
+/*
+//fetching data from api
+
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/jilgc`)
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
+  if (data)
+    return (
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    )
+  return <h1>Data</h1>;
+}
+
+export default App;
+*/
+
+
+
+
+
+/*
 import { useState } from 'react';
 import './App.css';
 
@@ -43,6 +114,8 @@ function App() {
 }
 
 export default App;
+
+*/
 
 
 
